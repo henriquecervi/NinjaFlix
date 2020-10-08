6566958613f353a9456f341fd67ad3e5b7e73b8e
Henrique Cervi Cutri <henrique.cutri@viavarejo.com.br>
2020-10-08 15:40:24 -0300

    Adicionando configuração -p browsers.

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,9 +10,20 @@ World(Helpers) #para ficar disponível global
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+case ENV["BROWSER"]
+when "firefox"
+    @diver = :selenium
+when "chrome"
+    @driver = :selenium_chrome
+when "headless"
+    @driver = :selenium_chrome_headless
+else
+    puts "Invalid Browser."
+end
+
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = @driver
     config.app_host = CONFIG["url"]
     config.default_max_wait_time = 10
 end
